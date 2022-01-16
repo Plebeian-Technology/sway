546e9e68cb5a33e9787d6faa6bfb093945cdbb37
dcordz <17937472+dcordz@users.noreply.github.com>
2022-01-16 15:09:09 -0500

    remove build dir, move cypress, gitignore

--- build.sh
+++ build.sh
@@ -7,15 +7,16 @@ APP=${1}
 if [[ "$APP" != "app-sway" && "$APP" != "app-widgets" ]]; then
     echo "First argument to start.sh must be an app name, either 'app-sway' or 'app-widgets'"
 else
-    echo "export * from \"./${APP}\";" > src/index.ts
+    echo "export * from \"./webapp\";" > src/index.ts
+    echo "export * from \"./${APP}\";" > src/webapp/index.ts
 
-    npx -C . craco build
+    react-scripts build
 
     if [[ "$APP" = "app-sway" ]]; then
         echo "Copying service-worker.js to public directory"
-        cp build/service-worker.js public/service-worker.js
+        cp src/build/service-worker.js public/service-worker.js
 
         echo "Copying service-worker.js.map to public directory"
-        cp build/service-worker.js.map public/service-worker.js.map
+        cp src/build/service-worker.js.map public/service-worker.js.map
     fi
 fi
