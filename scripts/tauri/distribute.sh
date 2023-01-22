92a90f745b5533ad07661c5a7ea3212acbf1a0d9
Legis Sway <legis@sway.vote>
2023-01-22 09:54:36 -0500

    tauri building

--- scripts/tauri/distribute.sh
+++ scripts/tauri/distribute.sh
@@ -1,13 +1,20 @@
 #!/usr/bin/env zsh
 
+set -eu
+
+cp ../apple/.env .apple.env
+sed -i '' '/^#/d' .apple.env
+export $(cat .apple.env | xargs)
+rm .apple.env
+
 echo ""
 echo "tauri - distribute.sh - Building for MacOS ARM 64 - M1 macs"
 echo ""
 
-tauri build --target aarch64-apple-darwin
+REACT_APP_TAURI=1 CI=true tauri build --target aarch64-apple-darwin --verbose
 
-echo ""
-echo "tauri - distribute.sh - Building for MacOS AMD 64 - Intel macs"
-echo ""
+# echo ""
+# echo "tauri - distribute.sh - Building for MacOS AMD 64 - Intel macs"
+# echo ""
 
-tauri build --target x86_64-apple-darwin
+# REACT_APP_TAURI=1 CI=true tauri build --target x86_64-apple-darwin --verbose
