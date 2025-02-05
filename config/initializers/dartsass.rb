c736b740c592c86ec55173d82c00ed4faf923c68
dcordz <17937472+dcordz@users.noreply.github.com>
2025-02-04 23:34:08 -0500

    padawan - rubocop formatting

--- config/initializers/dartsass.rb
+++ config/initializers/dartsass.rb
@@ -1,6 +1,7 @@
 # typed: strict
+
 # Rails.application.config.dartsass.builds = {
-  # "app/index.sass"  => "app.css",
-  # "site.scss"       => "site.css"
-  # "." => "."
+# "app/index.sass"  => "app.css",
+# "site.scss"       => "site.css"
+# "." => "."
 # }
--- lib/recaptcha_util.rb
+++ lib/recaptcha_util.rb
@@ -1,16 +1,16 @@
 class RecaptchaUtil
-  RECAPTCHA_URL = 'https://www.google.com/recaptcha/api/siteverify'
+  RECAPTCHA_URL = "https://www.google.com/recaptcha/api/siteverify"
 
   def self.valid?(token)
     return true if Rails.env.test?
 
     Faraday.post(RECAPTCHA_URL, {
-      body: "secret=#{ENV['GOOGLE_RECAPTCHA_SECRET_KEY']}&response=#{token}",
+      body: "secret=#{ENV["GOOGLE_RECAPTCHA_SECRET_KEY"]}&response=#{token}",
       headers: {
-        'Content-Type' => 'application/x-www-form-urlencoded',
-        'Charset' => 'utf-8',
-        'Accept' => 'application/json'
+        "Content-Type" => "application/x-www-form-urlencoded",
+        "Charset" => "utf-8",
+        "Accept" => "application/json"
       }
-    }).fetch('success', false)
+    }).fetch("success", false)
   end
 end
